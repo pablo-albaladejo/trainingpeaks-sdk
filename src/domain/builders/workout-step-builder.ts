9555507553a1f289d6dac81630ef5254c31a16b4
Pablo Albaladejo <pablo.albaladejo@aircall.io>
2025-07-31 11:33:09 +0200

    refactor: simplify state updates in WorkoutStepBuilder

--- src/domain/builders/workout-step-builder.ts
+++ src/domain/builders/workout-step-builder.ts
@@ -5,7 +5,7 @@ import { IntensityClass, LengthUnit } from '@/types';
  * Builder for workout structure steps
  */
 export class WorkoutStepBuilder {
-  readonly step: WorkoutStructureStep = {
+  private step: WorkoutStructureStep = {
     name: '',
     length: { value: 0, unit: LengthUnit.MINUTE },
     targets: [],
@@ -14,32 +14,38 @@ export class WorkoutStepBuilder {
   };
 
   name(name: string): this {
-    this.step.name = name;
+    this.step = { ...this.step, name };
     return this;
   }
 
   duration(minutes: number): this {
-    this.step.length = { value: minutes, unit: LengthUnit.MINUTE };
+    this.step = {
+      ...this.step,
+      length: { value: minutes, unit: LengthUnit.MINUTE },
+    };
     return this;
   }
 
   distance(value: number, unit: LengthUnit = LengthUnit.KILOMETER): this {
-    this.step.length = { value, unit };
+    this.step = { ...this.step, length: { value, unit } };
     return this;
   }
 
   addTarget(minValue: number, maxValue: number): this {
-    this.step.targets.push({ minValue, maxValue });
+    this.step = {
+      ...this.step,
+      targets: [...this.step.targets, { minValue, maxValue }],
+    };
     return this;
   }
 
   intensityClass(intensityClass: IntensityClass): this {
-    this.step.intensityClass = intensityClass;
+    this.step = { ...this.step, intensityClass };
     return this;
   }
 
   openDuration(open: boolean = true): this {
-    this.step.openDuration = open;
+    this.step = { ...this.step, openDuration: open };
     return this;
   }
 
