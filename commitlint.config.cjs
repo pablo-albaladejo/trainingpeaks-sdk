5fb01f345758b6ca625833c460ac13da7ce5a103
Pablo Albaladejo <pablo.albaladejo@aircall.io>
2025-08-11 22:06:44 +0200

    fix: re-enable body max line length in commitlint configuration

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  ignores: [(message) => message.startsWith('chore(release):')],
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
@@ -22,7 +23,6 @@ module.exports = {
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 72],
-    'body-max-line-length': [0, 'always', 100],
-    'footer-max-line-length': [0, 'always', 100],
+    'body-max-line-length': [2, 'always', 100],
   },
 };
